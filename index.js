47c0b634cce300959f9c9cbee76ea5657b0d0e17
Julien PALAS <julien.palas@gmail.com>
2023-07-12 15:24:40 +0200

    Update index.js sans axios

--- index.js
+++ index.js
@@ -1,67 +1,107 @@
 const express = require('express');
-const axios = require('axios');
+const http = require('http');
 
 const app = express();
-app.use(express.json()); // Pour parser les requêtes de type application/json
+app.use(express.json());
 
 // Remplacez par vos propres valeurs
 const GLPI_USERNAME = 'glpi_user';
 const GLPI_PASSWORD = 'glpi_password';
 
 // Route de proxy
-app.all('*', async (req, res) => {
+app.all('*', (req, res) => {
     // URL GLPI
     const glpiUrl = req.body.url_path;
 
     // Vérification de l'existence du session_token
     const sessionToken = req.body.session_token || null;
 
-    let token;
     if (!sessionToken) {
         // initSession
-        try {
-            const initRes = await axios.get(glpiUrl + '/initSession', {
-                auth: {
-                    username: GLPI_USERNAME,
-                    password: GLPI_PASSWORD,
-                },
-            });
-            token = initRes.data.session_token;
-        } catch (err) {
-            return res.status(500).send('Erreur lors de l\'initialisation de la session GLPI: ' + err.message);
-        }
-    } else {
-        token = sessionToken;
-    }
-
-    // Effectue la requête principale
-    try {
-        const mainRes = await axios({
-            method: req.method,
-            url: glpiUrl + req.path,
+        const initOptions = {
+            hostname: glpiUrl,
+            path: '/initSession',
+            method: 'GET',
             headers: {
-                'Session-Token': token,
-                ...req.headers,
-            },
-            data: req.body,
+                'Authorization': 'Basic ' + Buffer.from(GLPI_USERNAME + ':' + GLPI_PASSWORD).toString('base64')
+            }
+        };
+
+        const initReq = http.request(initOptions, (initRes) => {
+            let data = '';
+            initRes.on('data', (chunk) => {
+                data += chunk;
+            });
+            initRes.on('end', () => {
+                try {
+                    const parsedData = JSON.parse(data);
+                    const token = parsedData.session_token;
+                    proxyRequest(req, res, token, glpiUrl);
+                } catch (err) {
+                    return res.status(500).send('Erreur lors de l\'initialisation de la session GLPI: ' + err.message);
+                }
+            });
         });
 
-        // killSession
-        try {
-            await axios.get(glpiUrl + '/killSession', {
+        initReq.on('error', (error) => {
+            return res.status(500).send('Erreur lors de l\'initialisation de la session GLPI: ' + error.message);
+        });
+
+        initReq.end();
+    } else {
+        proxyRequest(req, res, sessionToken, glpiUrl);
+    }
+});
+
+function proxyRequest(originalReq, originalRes, token, glpiUrl) {
+    const options = {
+        hostname: glpiUrl,
+        path: originalReq.path,
+        method: originalReq.method,
+        headers: {
+            'Session-Token': token,
+            ...originalReq.headers,
+        },
+    };
+
+    const proxy = http.request(options, (res) => {
+        let data = '';
+        res.on('data', (chunk) => {
+            data += chunk;
+        });
+        res.on('end', () => {
+            // killSession
+            const killOptions = {
+                hostname: glpiUrl,
+                path: '/killSession',
+                method: 'GET',
                 headers: {
                     'Session-Token': token,
                 },
-            });
-        } catch (err) {
-            console.log('Erreur lors de la fermeture de la session GLPI: ' + err.message);
-        }
+            };
 
-        return res.json(mainRes.data);
-    } catch (err) {
-        return res.status(500).send('Erreur lors de la requête principale: ' + err.message);
+            const killReq = http.request(killOptions);
+            killReq.on('error', (error) => {
+                console.log('Erreur lors de la fermeture de la session GLPI: ' + error.message);
+            });
+
+            killReq.end();
+
+            // Send response
+            originalRes.json(JSON.parse(data));
+        });
+    });
+
+    proxy.on('error', (error) => {
+        return originalRes.status(500).send('Erreur lors de la requête principale: ' + error.message);
+    });
+
+    if (originalReq.method === 'POST' || originalReq.method === 'PUT') {
+        proxy.write(JSON.stringify(originalReq.body));
     }
-});
+
+    proxy.end();
+}
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Le serveur écoute sur le port ${PORT}`));
