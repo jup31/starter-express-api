357c9dd0e385c6aaf5d768c77447f8777f7ec04c
Julien Palas <julien.palas@wanadoo.fr>
2023-08-10 11:56:34 +0200

    Gestion des commentaires en/fr

--- index.js
+++ index.js
@@ -1,33 +1,46 @@
-const express = require('express');
+// en : This is the main file of the proxy server. It will be used to make requests to the GLPI API.
+// fr : Ceci est le fichier principal du serveur proxy. Il sera utilisé pour faire des requêtes à l'API GLPI.
+
+// en : Import the axios library to make HTTP requests
+// fr : Importe la bibliothèque axios pour faire des requêtes HTTP
 const axios = require('axios');
+// en : Import the express library to create the server
+// fr : Importe la bibliothèque express pour créer le serveur
+const express = require('express');
 
 const app = express();
 app.use(express.json());
 
-// Route de proxy
+
 app.all('*', async (req, res) => {
-    // Get the glpiUrl from the request and launch the correct error 400 if not found
+    // en : Get the glpiUrl from the request and launch the correct error 400 if not found
+    // fr : Récupère l'URL GLPI de la requête et lance l'erreur 400 si non trouvée
     const glpiUrl = req.header('url_path') || null;
     if (!glpiUrl) {
         return res.status(400).send("L'URL GLPI n'est pas fournie.");
     }
 
-    // Get the itemType from the request or set it to Computer by default
+    // en : Get the itemType from the request or set it to Computer by default
+    // fr : Récupère le type d'item de la requête ou le définit par défaut à Computer
     const itemType = req.header('itemType') || 'Computer';
     
-    // Check if a Session-Token is provided in the request (should not be provided)
+    // en : If sessionToken is null, request a new one using initSession GLPI API
+    // fr : Si le Session-Token est null, demande un nouveau en utilisant l'API initSession de GLPI
     const sessionToken = req.header('Session-Token') || null;
 
     if (!sessionToken) {
-        // If sessionToken is null, request a new one using initSession GLPI API
-        // Get the userToken and appToken from the request and launch the correct error 400 if not found
+        // en : If sessionToken is null, request a new one using initSession GLPI API
+        // en : Get the userToken and appToken from the request and launch the correct error 400 if not found
+        // fr : Si le Session-Token est null, demande un nouveau en utilisant l'API initSession de GLPI
+        // fr : Récupère le token d'utilisateur et le token d'application de la requête et lance l'erreur 400 si non trouvés
             const userToken = req.header('Authorization') || null;
             const appToken = req.header('App-Token') || null;
        
         if (!userToken || !appToken) {
             return res.status(400).send("Le token d'utilisateur ou le token d'application n'est pas fourni.");
         }
-
+        // en : GET request to initSession GLPI API
+        // fr : Requête GET à l'API initSession de GLPI
         try {
             const initResponse = await axios.get(
                 `https://${glpiUrl}/apirest.php/initSession?get_full_session=true`,
@@ -50,7 +63,8 @@ app.all('*', async (req, res) => {
     }
 });
 
-// Function to get the item list from GLPI API and send it back to the client
+// en : Function to get the item list from GLPI API and send it back to the client
+// fr : Fonction pour récupérer la liste des items depuis l'API GLPI et l'envoyer au client
 async function proxyRequest(originalReq, originalRes, sessionToken, glpiUrl, appToken) {
     const options = {
         url: `https://${glpiUrl}/apirest.php/${itemType}/?expand_dropdowns=true`,
@@ -65,7 +79,8 @@ async function proxyRequest(originalReq, originalRes, sessionToken, glpiUrl, app
     try {
         const proxyResponse = await axios(options);
 
-        // killSession
+        // en : GET request to killSession GLPI API
+        // fr : Requête GET à l'API killSession de GLPI
         const killOptions = {
             url: `https://${glpiUrl}/apirest.php/killSession`,
             method: 'GET',
@@ -75,15 +90,19 @@ async function proxyRequest(originalReq, originalRes, sessionToken, glpiUrl, app
                 'Session-Token': sessionToken,
             }
         };
-        // GET request to kill the session
+        // en : GET request to killSession GLPI API
+        // fr : Requête GET pour tuer la session
         await axios(killOptions);
 
-        // Send response
+        // en : Send the response back to the client
+        // fr : Envoie la réponse au client
         originalRes.json(proxyResponse.data);
     } catch (error) {
         return originalRes.status(500).send("Erreur lors de la requête principale: " + error.message);
     }
 }
 
+// en : Start the server
+// fr : Démarre le serveur
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Le serveur écoute sur le port ${PORT}`));
