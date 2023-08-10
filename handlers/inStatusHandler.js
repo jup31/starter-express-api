66338556db0ec5297b06652ab0ad5514c2f6d5f2
Julien Palas <julien.palas@wanadoo.fr>
2023-08-10 15:10:16 +0200

    Création de 2 endpoints pour séparer GLPI&Instatus

---
+++ handlers/inStatusHandler.js
@@ -0,0 +1,12 @@
+const axios = require('axios');
+
+// Le code pour traiter les requêtes vers inStatus
+async function handleRequest(req, res) {
+    
+    
+}
+
+module.exports = {
+    handleRequest
+};
+
--- index.js
+++ index.js
@@ -1,92 +1,22 @@
 // Ceci est le fichier principal du serveur proxy. Il sera utilisé pour faire des requêtes à l'API GLPI.
-
-const axios = require('axios');
+// Il est possible de lancer le serveur en utilisant la commande "node index.js" dans le terminal.
 const express = require('express');
+const glpiHandler = require('./handlers/glpiHandler'); // Importe le gestionnaire pour GLPI
+const inStatusHandler = require('./handlers/inStatusHandler'); // Importe le gestionnaire pour inStatus
 
 const app = express();
 app.use(express.json());
 
-
-app.all('*', async (req, res) => {
-    // Récupère l'URL GLPI de la requête et lance l'erreur 400 si non trouvée
-    const glpiUrl = req.header('url_path') || null;
-    if (!glpiUrl) {
-        return res.status(400).send("L'URL GLPI n'est pas fournie.");
-    }
-
-    // Récupère le type d'item de la requête ou le définit par défaut à Computer
-    const itemType = req.header('itemType') || 'Computer';
-    
-    // Si le Session-Token est null, demande un nouveau en utilisant l'API initSession de GLPI
-    const sessionToken = req.header('Session-Token') || null;
-
-    if (!sessionToken) {
-        // Si le Session-Token est null, demande un nouveau en utilisant l'API initSession de GLPI
-        // Récupère le token d'utilisateur et le token d'application de la requête et lance l'erreur 400 si non trouvés
-            const userToken = req.header('Authorization') || null;
-            const appToken = req.header('App-Token') || null;
-       
-        if (!userToken || !appToken) {
-            return res.status(400).send("Le token d'utilisateur ou le token d'application n'est pas fourni.");
-        }
-
-        // Requête GET à l'API initSession de GLPI
-        try {
-            const initResponse = await axios.get(
-                `https://${glpiUrl}/apirest.php/initSession?get_full_session=true`,
-                {
-                    headers: {
-                        'Content-Type': 'application/json',
-                        'Authorization': userToken,
-                        'App-Token': appToken,
-                    },
-                }
-            );
-
-            const sessionToken = initResponse.data.session_token;
-            await proxyRequest(req, res, token, glpiUrl, appToken);
-        } catch (err) {
-            return res.status(500).send("Erreur lors de l'initialisation de la session GLPI1: " + err.message);
-        }
-    } else {
-        await proxyRequest(req, res, sessionToken, glpiUrl, appToken);
-    }
+// Endpoint pour les requêtes GLPI
+app.all('/glpi', async (req, res) => {
+    // Traite la requête vers GLPI
+    await glpiHandler.handleRequest(req, res);  
 });
 
-// Fonction pour récupérer la liste des items depuis l'API GLPI et l'envoyer au client
-async function proxyRequest(originalReq, originalRes, sessionToken, glpiUrl, appToken) {
-    const options = {
-        url: `https://${glpiUrl}/apirest.php/${itemType}/?expand_dropdowns=true`,
-        method: originalReq.method,
-        headers: {
-            'Content-Type': 'application/json',
-            'App-Token': appToken,
-            'Session-Token': sessionToken,
-        },
-    };
-
-    try {
-        const proxyResponse = await axios(options);
-
-        // Requête GET à l'API killSession de GLPI
-        const killOptions = {
-            url: `https://${glpiUrl}/apirest.php/killSession`,
-            method: 'GET',
-            headers: {
-                'Content-Type': 'application/json',
-                'App-Token': appToken,
-                'Session-Token': sessionToken,
-            }
-        };
-        // Requête GET pour tuer la session
-        await axios(killOptions);
-
-        // Envoie la réponse au client
-        originalRes.json(proxyResponse.data);
-    } catch (error) {
-        return originalRes.status(500).send("Erreur lors de la requête principale: " + error.message);
-    }
-}
+// Endpoint pour les requêtes inStatus
+app.all('/instatus', async (req, res) => {
+    await inStatusHandler.handleRequest(req, res);
+});
 
 // Démarre le serveur
 const PORT = process.env.PORT || 3000;
